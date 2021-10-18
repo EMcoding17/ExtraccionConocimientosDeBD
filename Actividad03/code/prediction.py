34d3f509e33c6609bdfd295bea447cec24b309f3
Emma Romero <emmayrs17@gmail.com>
2021-10-18 00:54:35 +0000

    Creacion ó ejecucionódel modelolo de regresion

---
+++ Actividad03/code/prediction.py
@@ -0,0 +1,13 @@
+#PREDICCIONES
+
+#LIBRERIAS
+import sklearn
+from joblib import load
+
+model = load("model.joblib")
+while True:
+    xs = []
+    x = int(input("Valor de X: "))
+    xs.append([x])
+    prediction = model.predict(xs)
+    print(prediction)
